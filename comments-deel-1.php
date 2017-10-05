be28bb50d2435030d07fb418f21b1f0938d1dc7f
DaanKDG <daan.boschmans@student.kdg.be>
2017-10-05 11:24:07 +0200

    opdracht comment line toevoegen

---
+++ comments-deel-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+     <?php 
+    $voornaam = "Daan";
+    $achternaam = "Boschmans";
+     ?>
+     
+    <p> Ik ben : <?= $voornaam ?> <?= $achternaam ?></p>
+    
+</body>
+</html>
