d956ff31d8881011cc67c46c40f41a01aa52dee5
DaanKDG <daan.boschmans@student.kdg.be>
2017-10-11 13:26:48 +0200

    opdracht twee strings samenvoegen

---
+++ string-concatenate.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $voornaam = "Daan ";
+    $achternaam = "Boschmans";
+    $volledigeNaam = $voornaam . $achternaam;
+    
+    echo $volledigeNaam;
+    
+    ?>
+</body>
+</html>
